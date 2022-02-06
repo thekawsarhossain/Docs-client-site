3572e870cc320e7db0107ce6fda2568d137dd735
thekawsarhossain <thekawsarhossain@gmail.com>
2022-02-06 16:58:04 +0600

    testing

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h2 className="text-red-700">Hello World</h2>
+      <h2 className="text-blue-600">Testing by kawsar</h2>
     </div>
   )
 }
