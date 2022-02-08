87244e9e3dbb47740d5a578c8b5c3b6bf4ec6e2e
monirul-fahad <immonirulfahad@gmail.com>
2022-02-08 17:14:15 +0600

    fahad branch created

--- components/Home/Home/HomePage.js
+++ components/Home/Home/HomePage.js
@@ -1,9 +1,12 @@
+import VideoList from '../VideoList/VideoList'
+
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <VideoList />
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
