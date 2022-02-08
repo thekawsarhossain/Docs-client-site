338387b54ab40b45f12326c1a6469dcd8d627a1b
thekawsarhossain <thekawsarhossain@gmail.com>
2022-02-09 03:44:22 +0600

    communities component initial setup added

--- components/Home/Home/HomePage.js
+++ components/Home/Home/HomePage.js
@@ -1,6 +1,7 @@
 import Testimonial from '../Testimonial/Testimonial'
 import Navbar from '../../Shared/Navbar/Navbar'
 import Questions from '../Questions/Questions'
+import CommunitiesCard from '../CommunitiesCard/CommunitiesCard'
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <div>
         <Navbar />
         <Testimonial />
+        <CommunitiesCard />
         <Questions />
       </div>
     </>
