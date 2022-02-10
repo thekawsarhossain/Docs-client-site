302beb81f6fc405d715cbbabbe371533fadc5660
thekawsarhossain <thekawsarhossain@gmail.com>
2022-02-10 21:46:11 +0600

    code readed

--- components/Home/Home/HomePage.js
+++ components/Home/Home/HomePage.js
@@ -4,17 +4,19 @@ import Questions from '../Questions/Questions'
 import CommunitiesCard from '../CommunitiesCard/CommunitiesCard'
 import VideoList from '../VideoList/VideoList'
 import OnlineDoc from '../OnlineDoc/OnlineDoc'
+import Footer from '../Footer/Footer'
 
 const HomePage = () => {
   return (
-      <div>
-        <Navbar />
-        <CommunitiesCard />
-        <OnlineDoc/>
-        <VideoList />
-        <Questions />
-        <Testimonial />
-      </div>
+    <div>
+      <Navbar />
+      <CommunitiesCard />
+      <OnlineDoc />
+      <VideoList />
+      <Questions />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
