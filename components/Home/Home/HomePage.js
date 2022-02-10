a2ea946147b39b5782d7a2d0bad07c2de09e0fa3
thekawsarhossain <thekawsarhossain@gmail.com>
2022-02-10 23:49:10 +0600

    all set

--- components/Home/Home/HomePage.js
+++ components/Home/Home/HomePage.js
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      {/* <HeroSection /> */}
       <VideoList />
       <Questions />
       <CommunitiesCard />
