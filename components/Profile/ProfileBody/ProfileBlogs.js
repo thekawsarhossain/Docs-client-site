1392e850fa5902dfe5fa8d7da3d205fc08185dd0
MdBayazid-star <freelencerbayazid@gmail.com>
2022-02-18 04:56:50 -0500

    Profile Section

---
+++ components/Profile/ProfileBody/ProfileBlogs.js
@@ -0,0 +1,260 @@
+const ProfileBlogs = () => {
+  return (
+    <div>
+      <div className="scisco-masonry-grid" style={{ opacity: '1' }}>
+        <div className="scisco-two-columns" data-columns="2">
+          <div className="scisco-column size-1of2">
+            <div className="post-32 post type-post status-publish format-standard has-post-thumbnail hentry category-business category-marketing tag-business tag-company tag-office tag-technology">
+              <div className="card-masonry">
+                <div className="card">
+                  <a
+                    className="card-featured-img"
+                    href="https://wordpress-288344-1596643.cloudwaysapps.com/how-can-my-business-win-awards/"
+                  >
+                    <img
+                      className="card-img-top"
+                      src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/9-1024x683.jpg"
+                      alt="How Can My Business Win Awards?"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/how-can-my-business-win-awards/">
+                        How Can My Business Win Awards?
+                      </a>
+                    </h3>
+                    <p>
+                      Qui ex graviterque si et velit quamquam erudit. Amet
+                      quamquam eu consequat. Quo cupidatat volupta, appellat
+                      anim ab litteris domestic…
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <div className="card-avatar">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                        <div className="scisco-verified">
+                          <i className="fas fa-check" title="Verified user"></i>
+                          <img
+                            className="avatar"
+                            src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/avatar-200x200.jpg"
+                            width="80"
+                            height="80"
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </div>
+                    <div className="card-meta">
+                      <div className="card-meta-author">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                          John Doe
+                        </a>
+                      </div>
+                      <div className="card-meta-date">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/how-can-my-business-win-awards/">
+                          March 12, 2019
+                        </a>
+                      </div>
+                    </div>
+                    <div className="card-comments">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/how-can-my-business-win-awards//#scisco-comments-wrapper">
+                        <i className="fas fa-comments"></i>3
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="post-25 post type-post status-publish format-standard has-post-thumbnail hentry category-business category-finance tag-business tag-finance tag-office">
+              <div className="card-masonry">
+                <div className="card">
+                  <a
+                    className="card-featured-img"
+                    href="https://wordpress-288344-1596643.cloudwaysapps.com/gdpr-misconceptions-explained/"
+                  >
+                    <img
+                      className="card-img-top"
+                      src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/7-1024x683.jpg"
+                      alt="GDPR Misconceptions Explained"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/gdpr-misconceptions-explained/">
+                        GDPR Misconceptions Explained
+                      </a>
+                    </h3>
+                    <p>
+                      Ea sint tempor non tempor veniam sed deserunt relinqueret,
+                      culpa probant aut tamen ipsum ut incididunt elit cupidatat
+                      commodo e lorem occaecat…
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <div className="card-avatar">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                        <div className="scisco-verified">
+                          <i className="fas fa-check" title="Verified user"></i>
+                          <img
+                            className="avatar"
+                            src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/avatar-200x200.jpg"
+                            width="80"
+                            height="80"
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </div>
+                    <div className="card-meta">
+                      <div className="card-meta-author">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                          John Doe
+                        </a>
+                      </div>
+                      <div className="card-meta-date">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/gdpr-misconceptions-explained/">
+                          January 27, 2019
+                        </a>
+                      </div>
+                    </div>
+                    <div className="card-comments">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/gdpr-misconceptions-explained//#scisco-comments-wrapper">
+                        <i className="fas fa-comments"></i>0
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="scisco-column size-1of2">
+            <div className="post-30 post type-post status-publish format-gallery has-post-thumbnail hentry category-finance category-technology tag-business tag-office tag-technology post_format-post-format-gallery">
+              <div className="card-masonry">
+                <div className="card">
+                  <a
+                    className="card-featured-img"
+                    href="https://wordpress-288344-1596643.cloudwaysapps.com/a-new-era-of-technology/"
+                  >
+                    <img
+                      className="card-img-top"
+                      src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/4-1024x683.jpg"
+                      alt="A New Era of Technology"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/a-new-era-of-technology/">
+                        A New Era of Technology
+                      </a>
+                    </h3>
+                    <p>
+                      Sunt eiusmod sed ingeniis iis quamquam compre in arbitror
+                      eu sed irure aute summis cernantur et et amet incurreret
+                      cohae, ubi ab familiar…
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <div className="card-avatar">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                        <div className="scisco-verified">
+                          <i className="fas fa-check" title="Verified user"></i>
+                          <img
+                            className="avatar"
+                            src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/avatar-200x200.jpg"
+                            width="80"
+                            height="80"
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </div>
+                    <div className="card-meta">
+                      <div className="card-meta-author">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                          John Doe
+                        </a>
+                      </div>
+                      <div className="card-meta-date">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/a-new-era-of-technology/">
+                          February 19, 2019
+                        </a>
+                      </div>
+                    </div>
+                    <div className="card-comments">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/a-new-era-of-technology//#scisco-comments-wrapper">
+                        <i className="fas fa-comments"></i>1
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="post-18 post type-post status-publish format-standard has-post-thumbnail hentry category-finance tag-business tag-commerce">
+              <div className="card-masonry">
+                <div className="card">
+                  <a
+                    className="card-featured-img"
+                    href="https://wordpress-288344-1596643.cloudwaysapps.com/heres-which-commerce-markets-to-watch/"
+                  >
+                    <img
+                      className="card-img-top"
+                      src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/2-1024x683.jpg"
+                      alt="Commerce Markets to Watch"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h3 className="card-title">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/heres-which-commerce-markets-to-watch/">
+                        Commerce Markets to Watch
+                      </a>
+                    </h3>
+                    <p>
+                      Nostrud quis ne excepteur praetermissum, probant non
+                      probant, nulla cernantur nostrud ab expetendis irure
+                      senserit, id magna aute velit iudicem ex quem…
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <div className="card-avatar">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                        <div className="scisco-verified">
+                          <i className="fas fa-check" title="Verified user"></i>
+                          <img
+                            className="avatar"
+                            src="https://wordpress-288344-1596643.cloudwaysapps.com/wp-content/uploads/2020/11/avatar-200x200.jpg"
+                            width="80"
+                            height="80"
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </div>
+                    <div className="card-meta">
+                      <div className="card-meta-author">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/author/egemenerd/">
+                          John Doe
+                        </a>
+                      </div>
+                      <div className="card-meta-date">
+                        <a href="https://wordpress-288344-1596643.cloudwaysapps.com/heres-which-commerce-markets-to-watch/">
+                          January 14, 2019
+                        </a>
+                      </div>
+                    </div>
+                    <div className="card-comments">
+                      <a href="https://wordpress-288344-1596643.cloudwaysapps.com/heres-which-commerce-markets-to-watch//#scisco-comments-wrapper">
+                        <i className="fas fa-comments"></i> 0
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileBlogs
