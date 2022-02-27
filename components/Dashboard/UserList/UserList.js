9bdce52d1bd3558a16fa58e32361305450c4b64b
Rakibul santo <SANTODON176@gmail.com>
2022-02-28 00:37:24 +0600

    add DashBoard

---
+++ components/Dashboard/UserList/UserList.js
@@ -0,0 +1,9 @@
+const UserList = () => {
+  return (
+    <div>
+      <h1>UserList</h1>
+    </div>
+  );
+}
+
+export default UserList;
---
+++ components/Dashboard/UserProfile/UserProfile.js
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+  return (
+    <div>
+     <h1> UserProfile</h1>
+    </div>
+  );
+}
+
+export default UserProfile;
