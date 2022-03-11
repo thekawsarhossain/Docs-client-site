61270030f8a4d4cb5ff1be1b73ecf85252a8e5a3
shihab-2021 <shihab77023@gmail.com>
2022-03-11 11:36:02 +0600

    for solving bugs :(

--- pages/login.js
+++ pages/login.js
@@ -7,6 +7,7 @@ const login = () => {
         <title>Login</title>
       </Head>
       {/* login component called here  */}
+      {/* Also register component will call from here */}
       <Login />
     </div>
   )
