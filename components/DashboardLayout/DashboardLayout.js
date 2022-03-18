486d4adf65ac9635a521cc78838a96a77160a609
MdBayazid-star <freelencerbayazid@gmail.com>
2022-03-18 06:28:34 -0400

    Dashboard Updating

--- components/DashboardLayout/DashboardLayout.js
+++ components/DashboardLayout/DashboardLayout.js
@@ -143,7 +143,6 @@ function DashboardLayout(props) {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
           variant="temporary"
