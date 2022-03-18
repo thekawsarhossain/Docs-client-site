8dea1fa3574b091f0660e41845629c03bf79c64a
Kawsar Hossain <86672839+thekawsarhossain@users.noreply.github.com>
2022-03-18 11:04:55 +0600

    report blog, followers option all issue fixed

---
+++ messages/ReportReason.js
@@ -0,0 +1,10 @@
+const REPORT_REASON = {
+  'For breaking the first rule': '1 - For breaking the first rule',
+  'For breaking the second rule': '2 - For breaking the second rule',
+  'For breaking the third rule': '3 - For breaking the third rule',
+  'For breaking the fourth rule': '4 - For breaking the fourth rule',
+  'For breaking the fifth rule': '5 - For breaking the fifth rule',
+  'something else': '6 - something else',
+}
+
+export default REPORT_REASON
